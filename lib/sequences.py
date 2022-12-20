d098b5c942ada17800f724f48a59f3a5d04dbab6
Lucas Duncan <lucasdonuts@gmail.com>
2022-12-20 16:24:00 -0600

    All tests passing

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print(list())
+        return
+    elif length == 1:
+        print([0])
+        return
+
+    fib = [0, 1]
+    i = 2
+
+    while i < length:
+        fib.append(fib[len(fib) - 1] + fib[len(fib) - 2])
+        i += 1
+
+    print(fib)
+
+print_fibonacci(0)
+print_fibonacci(1)
+print_fibonacci(6)
+print_fibonacci(10)
